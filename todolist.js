796ce42fb985e96fda8fb0c042f9b9a86a927656
ludovic <spestlrosh@gmail.com>
2018-01-25 19:01:56 +0100

    modif1

---
+++ todolist.js
@@ -0,0 +1,15 @@
+// todoList.js
+'use strict';
+
+
+/**
+ * Déclaration de l'application demoApp
+ */
+var demoApp = angular.module('demoApp', [
+    // Dépendances du "module"
+    'todoList'
+]);
+/**
+ * Déclaration du module todoList
+ */
+var todoList = angular.module('todoList',[]);
